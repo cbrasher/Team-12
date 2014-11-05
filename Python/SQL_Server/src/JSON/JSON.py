de1131c85c1bcf5c8997aa9ca8fb216c551b73a7
cbrasher <cbrasher@tamu.edu>
2014-11-05 17:20:36 -0600

    JSON and Google Fusion Table Update
    Got JSON format to push to SQL Database
    Started working with Google Fusion Table API

--- Python/SQL_Server/src/JSON/JSON.py
+++ Python/SQL_Server/src/JSON/JSON.py
@@ -1,14 +1,5 @@
-'''
-Created on Oct 17, 2014
 
-@author: Owner
-'''
-
-import json
-import sqlite3
-import threading, Queue
-import serial
-
+''' Ignore this for now
 def _decode_list(data):
     rv = []
     for item in data:
@@ -35,7 +26,16 @@ def _decode_dict(data):
         rv[key] = value
     return rv
 
-d = {'Sensor1':'17.12', 'Sensor2': '85.12', 'Sensor3':'45.63'}
+d = { "Sensor1": 17.12, "Sensor2": 85.12, "Sensor3": 45.63}
+a = []
+b = 0
+if b < 10:
+    a.append(d)
+print a
+for b in range(0,1):
+    b += 1
+    a.pop()
+print a
 j = json.dumps(d, sort_keys=True)
 i = json.loads(j,object_hook=_decode_dict)
 y= json.dumps(i)
@@ -51,19 +51,55 @@ p = "23.87"
 g = (h,m,n,y,u,p)
 print j
 print i
+print a
+
+'''
+import json
+import sqlite3
+import random
+from time import sleep
 
 db = sqlite3.connect("Test.sqlite")
 cursor = db.cursor()
+#cursor.execute('''Drop TABLE tester''')
+db.commit()
 cursor.execute('''CREATE TABLE IF NOT EXISTS
                       tester(id INTEGER PRIMARY KEY, Sensor_Name TEXT, Sensor_Value REAL, Sensor_Name1 TEXT, Sensor_Value1 REAL, Sensor_Name2 TEXT, Sensor_Value2 REAL)''')
-
-
-cursor.execute('INSERT INTO tester (Sensor_Name, Sensor_Value,Sensor_Name1, Sensor_Value1,Sensor_Name2, Sensor_Value2) VALUES (?,?,?,?,?,?)',k)
 db.commit()
-query = 'INSERT INTO tester (Sensor_Name, Sensor_Value,Sensor_Name1, Sensor_Value1,Sensor_Name2, Sensor_Value2) VALUES (?,?,?,?,?,?)'
-columns = ['Sensor_Name','Sensor_Value','Sensor_Name1','Sensor_Value1','Sensor_Name2','Sensor_Value2']
-for data in i.iteritems():
-    keys = (data[c] for c in columns)
-    cursor.execute(query,keys)
-db.commit()
-db.close()
+i = 0
+str1 = '"Sensor1"'
+try:
+    while i<=1000:
+        a = random.randrange(0.00,500.00)
+        b = random.randrange(0.00,500.00)
+        c = random.randrange(0.00,500.00)
+    
+        data = '{' '"id1"' + ' : ' + str1 + ','+ '"Sensor1"' + ' : '+ str(a) +','+ '"id2"' + ' : ' + '"Sensor2"' + ',' + '"Sensor2"' + ' : '+ str(b)+ ',' + '"id3"' + ' : ' + '"Sensor3"' + ','  + '"Sensor3"' + ' : ' + str(c)+'}'
+
+        decode_d2 = json.loads(data)
+        b0 = decode_d2["id1"]
+        b1 = decode_d2[b0]
+        b2 = decode_d2["id2"]
+        b3 = decode_d2[b2]
+        b4 = decode_d2["id3"]
+        b5 = decode_d2[b4]
+        list1 = [b0, b1, b2, b3, b4, b5]
+    
+        '''d0 = '{"id1" : "Sensor1" ,"Sensor1" : 17.12, "id2" : "Sensor2", "Sensor2" : 85.12, "id3" : "Sensor3", "Sensor3": 45.63}'
+        decode_d0 = json.loads(d0) #need to give json parameter in decode to seperate terms into respective fields 
+        a0 = decode_d0["id1"]
+        a1 = decode_d0[a0]
+        a2 = decode_d0["id2"]
+        a3 = decode_d0[a2]
+        a4 = decode_d0["id3"]
+        a5 = decode_d0[a4]
+        list0 = [a0, a1, a2, a3, a4 ,a5]
+        print list0'''
+    
+        cursor.execute('INSERT INTO tester (Sensor_Name, Sensor_Value,Sensor_Name1, Sensor_Value1,Sensor_Name2, Sensor_Value2) VALUES (?,?,?,?,?,?)',list1)
+        db.commit()
+        print "Computing"
+        i += 1
+finally:
+    print 'Done'
+    db.close()
---
+++ Python/SQL_Server/src/Tutorial_JSON/Tutorial_JSON.py
@@ -0,0 +1,31 @@
+'''
+Created on Nov 5, 2014
+
+@author: Owner
+'''
+import demjson
+
+data = [ { 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 } ]
+
+json = demjson.encode(data)
+print json
+
+text = demjson.decode(json)
+print text
+
+import json
+
+# fix this function, so it adds the given name
+# and salary pair to salaries_json, and return it
+def add_employee(salaries_json, name, salary):
+    # Add your code here
+
+    return salaries_json
+
+# test code
+salaries = '{"Alfred" : 300, "Jane" : 400 }'
+new_salaries = add_employee(salaries, "Me", 800)
+decoded_salaries = json.loads(new_salaries)
+print decoded_salaries["Alfred"]
+print decoded_salaries["Jane"]
+print decoded_salaries["Me"]
